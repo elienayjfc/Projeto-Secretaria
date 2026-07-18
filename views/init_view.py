dd6970c946f6d7e721e5f640d7077eb17a5b4d60
elienayjfc <elienayjfc@hotmail.com>
2026-07-18 12:54:02 -0300

    intergração login com tela inicial
    Co-authored-by: Luiz Felipe <LuizFelpsc@users.noreply.github.com>

--- views/init_view.py
+++ views/init_view.py
@@ -1,32 +1,37 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
+
 #Janela inicial Progama
+def tela_init():
+    root = tk.Tk()
+    root.title("ADJG")
+    frm = tk.Frame(root)
+    root.geometry('800x500')
+    frm.pack(side='left', anchor='n')
 
-root = tk.Tk()
-frm = tk.Frame(root)
-root.geometry('800x500')
-frm.pack(side='left', anchor='n')
+    #Menu Congregações
 
-#Menu Congregações
+    congregacao = tk.Menubutton(frm, text = 'Congregações', relief = 'raised', padx= 40, pady=20)
+    congregacao.pack(side=tk.LEFT)
+    menu_congregacao= tk.Menu(congregacao, tearoff=0)
+    menu_congregacao.add_command(label='Relatorios gerencial', command=lambda: messagebox.showinfo('Aviso', 'Novo criado'))
+    menu_congregacao.add_command(label='Relatorio de cultos ', command=lambda: messagebox.showinfo('Aviso', 'Novo criado'))
+    menu_congregacao.add_command(label='futuro ...          ', command=lambda: messagebox.showinfo('Aviso', 'Novo criado'))
+    congregacao.config(menu = menu_congregacao)
 
-congregacao = tk.Menubutton(frm, text = 'Congregações', relief = 'raised', padx= 40, pady=20)
-congregacao.pack(side=tk.LEFT)
-menu_congregacao= tk.Menu(congregacao, tearoff=0)
-menu_congregacao.add_command(label='Relatorios gerencial', command=lambda: messagebox.showinfo('Aviso', 'Novo criado'))
-menu_congregacao.add_command(label='Relatorio de cultos ', command=lambda: messagebox.showinfo('Aviso', 'Novo criado'))
-menu_congregacao.add_command(label='futuro ...          ', command=lambda: messagebox.showinfo('Aviso', 'Novo criado'))
-congregacao.config(menu = menu_congregacao)
+    #Menu Membros
 
-#Menu Membros
-
-membros = tk.Menubutton(frm, text='Membros', relief = 'raised', padx= 40, pady=20)
-membros.pack(side=tk.LEFT)
-menu_membros = tk.Menu(membros, tearoff=0)
-menu_membros.add_command(label='Adicionar membros ')
-menu_membros.add_command(label='Situação cadastral')
-menu_membros.add_command(label='Remover membros   ')
-membros.config(menu = menu_membros)
+    membros = tk.Menubutton(frm, text='Membros', relief = 'raised', padx= 40, pady=20)
+    membros.pack(side=tk.LEFT)
+    menu_membros = tk.Menu(membros, tearoff=0)
+    menu_membros.add_command(label='Adicionar membros ')
+    menu_membros.add_command(label='Situação cadastral')
+    menu_membros.add_command(label='Remover membros   ')
+    membros.config(menu = menu_membros)
 
 
-root.mainloop()
+    root.mainloop() 
+
+#tela_init()
--- views/login_view.py
+++ views/login_view.py
@@ -1,19 +1,31 @@
 from tkinter import *
 from tkinter import ttk
 
-def telaLogin(callback_login): 
-    root = Tk()
-    tela = ttk.Frame(root, padding= 50)
-    root.title("Login")
-    tela.grid()
+#def tela():
+root = Tk()
+root.title("Login")
 
-    ttk.Label(tela, text= "Usuario:").grid(column=0,row=0)
-    login_view = ttk.Entry(tela, text= "usuario: ")
+frame = ttk.Frame(root, padding=50)
+frame.grid()
+#return root, frame
+
+
+def telaLogin(callback_login):
+    #root, frame = tela()
+    ttk.Label(frame, text="Usuário:").grid(column=0, row=0)
+    login_view = ttk.Entry(frame)
     login_view.grid(column=1, row=0)
-    ttk.Label(tela, text= "Senha").grid(column=0, row = 1)
-    senha_view = ttk.Entry(tela, show="*")
+    ttk.Label(frame, text="Senha").grid(column=0, row=1)
+    senha_view = ttk.Entry(frame, show="*")
     senha_view.grid(column=1, row=1)
-    entrar = ttk.Button(tela, text="Entrar", command=lambda: callback_login(login_view.get(), senha_view.get()))
-    entrar.grid(column=1, row=2)
+
+    entrar = ttk.Button(
+        frame,text="Entrar",
+        command=lambda: callback_login(login_view.get(), senha_view.get()))
     
-    return tela
+    entrar.grid(column=1, row=2)
+    return root
+
+def fechar_tela():
+    root.destroy()
+
